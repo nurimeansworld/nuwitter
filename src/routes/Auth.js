ed02bfc5ee481aef600ad635dddbe1870a1d2eea
nurimeansworld <nurimeansworld@gmail.com>
2022-03-08 17:10:28 +0900

    Add: 로그인 폼 html 및 value 관련 이벤트

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [userEmail, setuserEmail] = useState('');
+  const [userPw, setuserPw] = useState('');
+
+  const onChange = (e) => {
+    // e로부터 name, value를 받아오기
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'userEmail') {
+      setuserEmail(value);
+      // console.log('userEmail', value);
+    } else if (name === 'userPw') {
+      setuserPw(value);
+      // console.log('userPw', value);
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('onSubmit');
+  };
+
+  return (
+    <>
+      <h2>Auth</h2>
+      <form onSubmit={onSubmit}>
+        <label htmlFor='userEmail'>email</label>
+        <input
+          type='text'
+          placeholder='email@email.com'
+          name='userEmail'
+          required
+          onChange={onChange}
+        />
+        <label htmlFor='userPw'>password</label>
+        <input
+          type='password'
+          placeholder='password'
+          name='userPw'
+          required
+          onChange={onChange}
+        />
+        <input type='submit' value='login' />
+      </form>
+      <section>
+        <button>Continue with Google</button>
+        <button>Continue with GitHub</button>
+      </section>
+    </>
+  );
+};
 export default Auth;
