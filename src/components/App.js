fa22cbf7c207de192d4c21bdcc82f509d599c1f1
nurimeansworld <nurimeansworld@gmail.com>
2022-03-08 16:13:36 +0900

    Add: jsconfig.json으로 절대경로 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import AppRouter from './Router';
-import { authService } from '../fb';
-console.log('authService', authService);
+import AppRouter from 'components/Router';
+import { authService } from 'fb';
+// console.log('authService', authService);
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
 
   return <AppRouter isLoggedIn={isLoggedIn} />;
 }
